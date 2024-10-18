2cd636f70159301b66c283e48146827c3257d427
fencim <fencim@gmail.com>
2024-10-18 10:05:07 +0800

    setup pirate compo

---
+++ src/components/PirateComp.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Pirate: {{ teamName }}</h1>
+    <ul>
+        <li v-for="m in members" :key="m.name" >{{ m.name }} ({{ m.role }})</li>
+    </ul>
+</template>
+<script setup lang="ts">
+    import { ref } from "vue";
+    interface IMember {
+        name: string;
+        role: string;
+    }
+    const teamName = "Straw Hats";
+    const members = ref<IMember[]>([{
+        name: "Luffy", role: 'Captain'
+    },{
+        name: "Zoro", role: 'Right Hand'
+    }])
+</script>
