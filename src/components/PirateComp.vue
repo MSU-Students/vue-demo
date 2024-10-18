dba52e9b934162ee23531cb62111cc2f593e4b8f
fencim <fencim@gmail.com>
2024-10-18 10:15:01 +0800

    register new member

--- src/components/PirateComp.vue
+++ src/components/PirateComp.vue
@@ -1,5 +1,10 @@
 <template>
     <h1>Pirate: {{ teamName }}</h1>
+    <div>
+        <input type="text" v-model="newMember.name" placeholder="Name" />
+        <input type="text" v-model="newMember.role" placeholder="Role" />
+        <button @click="onRegister">Register</button>
+    </div>
     <ul>
         <li v-for="m in members" :key="m.name" >{{ m.name }} ({{ m.role }})</li>
     </ul>
@@ -10,10 +15,18 @@
         name: string;
         role: string;
     }
+    const newMember = ref<IMember>({
+        name: '', role: ''
+    });
     const teamName = "Straw Hats";
     const members = ref<IMember[]>([{
         name: "Luffy", role: 'Captain'
     },{
         name: "Zoro", role: 'Right Hand'
     }])
+    function onRegister() {
+        members.value.push({...newMember.value});
+        newMember.value.name = '';
+        newMember.value.role = '';
+    }
 </script>
