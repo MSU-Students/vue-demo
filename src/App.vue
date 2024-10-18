534c548f978849b466fe9d0d4d1f523ad8687cfd
fencim <fencim@gmail.com>
2024-10-18 09:49:37 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import { ref } from "vue";
+  const counter = ref(1);
+  const formTitle = 'Demo lang to';
+</script>
+
+<template>
+  <header>
+    <h1>Practice Pirate</h1>
+  </header>
+  <main>
+    {{ formTitle }}
+    {{ counter }}
+    <input type="text" v-model="counter" />
+    <button @click="counter++">Increment</button>
+    <button @click="counter--">Decrement</button>
+  </main>
+</template>
